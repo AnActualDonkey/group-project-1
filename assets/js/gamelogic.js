5e554f209981742bec8b8c71bda0cf395f57ca80
Phillip Booker <phillippbooker@gmail.com>
2019-04-26 23:42:42 -0700

    Starts API call function.

--- assets/js/gamelogic.js
+++ assets/js/gamelogic.js
@@ -1,10 +1,4 @@
-//https://superheroapi.com/api/384939068772291/character-id
-//https://superheroapi.com/api/384939068772291/character-id/powerstats
-
-
-console.log(testVar + myVar);
-
-
+var superHeroCode = "384939068772291";
 
 
 var gameHealth;
@@ -59,31 +53,27 @@ function startButton(){
         //start timer for fight phase
         gameState = "fight";
     }
+}
 
 function send() {
-    var urlvariable;
+    //URL for SuperHero DB (not working)
+    //https://superheroapi.com/api/384939068772291/character-id
+    //https://superheroapi.com/api/384939068772291/character-id/powerstats
 
-    urlvariable = "Character ID";
+    //URL for Marvel
+    // https://gateway.marvel.com:443/v1/public/characters?apikey=
 
-    var ItemJSON;
+    var queryUrl = "https://superheroapi.com/api/" + superHeroCode + "/character-id";
 
-    ItemJSON = '[{"Full Name"}]';
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    });
 
-    URL = "https://testrestapi.com/additems?var=" + urlvariable; "https://superheroapi.com/api/384939068772291/character-id"
-
-
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = callbackFunction(xmlhttp);
-    xmlhttp.open("POST", URL, false);
-    xmlhttp.setRequestHeader("Superhero API", "superheroapi.json");
-    xmlhttp.setRequestHeader('Authorization', 'Basic ' + window.btoa('384939068772291'));
-    xmlhttp.onreadystatechange = callbackFunction(xmlhttp);
-    xmlhttp.send(ItemJSON);
-    alert(xmlhttp.responseText);
-    document.getElementById("#gamearea").innerHTML = xmlhttp.statusText + ":" + xmlhttp.status + "<textarea rows='100' cols='100'>" + xmlhttp.responseText + "</textarea>";
+    
 }
 
-function callbackFunction(xmlhttp) {
-    //alert(xmlhttp.responseXML);
-
-}
+//Calls API function
+send();
