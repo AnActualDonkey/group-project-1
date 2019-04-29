1a7aa524abc76c1b0f9a6ceecc2bd1ffbfcbb3b1
Phillip Booker <phillippbooker@gmail.com>
2019-04-28 19:34:24 -0700

    Adds logic for firebase connections and game state logic.

--- assets/js/gamelogic.js
+++ assets/js/gamelogic.js
@@ -2,17 +2,26 @@ var fbKey = "AIzaSyChz5y9l2HLc7BpjOxXQAt3R1mjSH0tA3A";
 var marvelPublicCode = "9c9ee8837ea5626e53f61a1af4ddf211";
 
 //these variables are for the game logic
-var gameHealth;
-var gameUser;
-var gameKey;
+
+var playerName;
+var playerKey;
+var playerClicks;
+var playerTeam;
+
+var gameState = "initial";
 var gameHost;
 var gameHeroName;
 var gameHeroes = [];
 var gameState;
 var gameClicks;
 
+//Firebae variables
+var database;
+var gameRef;
+var chatRef;
+var connectionsRef;
+var connectedRef;
 
-//Initialize Firebase
 // Initialize Firebase
 var config = {
     apiKey: fbKey,
@@ -24,17 +33,111 @@ var config = {
 };
 firebase.initializeApp(config);
 
+database = firebase.database();
+console.log(database);
+
+gameRef = database.ref("/game");
+connectionsRef = database.ref("connections");
+connectedRef = database.ref(".info/connected");
+
+
+connectedRef.on("value", function(snapshot) {
+    // If they are connected..
+    if (snapshot.val()) {
+
+        // Add user to the connections list.
+        playerKey = connectionsRef.push(true);
+
+        // chatRef.set({
+        //     chat: screenName + " has joined the chat!",
+        //     sender: "System"
+        // });
+
+        // Remove user from the connection list when they disconnect.
+        playerKey.onDisconnect().remove();
+
+        // for (key in playerKey){
+        //     console.log(key);
+        // }
+
+        console.log("Connection id: " + playerKey);
+        console.log("Type: " + playerKey.key);
+    }
+}, function(errorObject){
+    console.log(errorObject.code);
+});
+
+connectionsRef.on("value", function(snapshot){
+    if((gameState === "initial")){
+        var teamCount = 0;
+        var childKeys = [];
+        console.log(snapshot.child);
+        snapshot.forEach(function(child) {
+            childKeys.push(child.key);
+            teamSet = (teamCount % 2) + 1;
+            database.ref("/connections/" + child.key).set({
+                team: teamSet
+            });
+            
+            if(child.key === playerKey.key){
+                playerTeam = teamSet;
+            }
+            teamCount++;
+        });
+    }
+});
+
 //game ref
 //update hostId here upon game value changes
 //Handle when hero health is below zero (change state to done)
+//If either team health is 0, change game state to end
+//Then update()
+gameRef.on("value", function(snapshot){
+    gameState = snapshot.val().gameState;
+    gameHost = snapshot.val().host;
+    teamHero1 = snapshot.val().hero1;
+    teamHero2 = snapshot.val().hero2;
+    teamHealth1 = snapshot.val().health1;
+    teamHealth2 = snapshot.val().health2;
 
+    if((playerKey.key === gameHost) && (gameState === "initial")){
+        //Make start button visible
+    }
+
+    if(gameState === "fight"){
+        if((teamHealth1 <= 0) || (teamHealth2 <= 0)){
+            gameState = "done";
+            updateGameDb(teamHealth1, teamHealth2, teamHero1, teamHero2, gameHost, gameState);
+            //Need to set game back to initial state
+            //Soft reset function
+        }
+    }
+})
+
+function checkGame(){
+    console.log("Health 1:" + teamHealth1);
+    console.log("Health 2:" + teamHealth2);
+    console.log("Team:" + playerTeam);
+}
 
 function resetGame() {
-    gameState = "start";
-    gameHealth = 0;
+    gameState = "initial";
+    
+    playerClicks = 0;
+
+    teamHealth1 = 10;
+    teamHero1 = "";
+    
+    teamHealth2 = 10;
+    teamHero2 = "";
+
+    gameHost = "no-host-entered";
+
     gameUser = "";
     gameKey = "";
     gameClicks = 0;
+
+    updateGameDb(teamHealth1, teamHealth2, teamHero1, teamHero2, gameHost, gameState);
 }
 
 
@@ -47,8 +150,16 @@ function setName() {
 }
 
 
-function updateGameDb() {
+function updateGameDb(health1, health2, hero1, hero2, host, state) {
     //Updates DB with who is host and other game values
+    gameRef.set({
+        health1: health1,
+        health2: health2,
+        hero1: hero1,
+        hero2: hero2,
+        host: host,
+        gameState: state
+    })
 }
 
 function attackButton() {
@@ -61,16 +172,7 @@ function attackButton() {
 
 
 function startButton() {
-    if (gameState === "start" && gameKey === gameHost) {
-
-        //begin timer for fight phase
-        //wait
-        //choose hero
-        //set game values
-        //update server game values
-        //start timer for fight phase
-        gameState = "fight";
-    }
+    
 }
 
 function send() {//URL for Marvel
@@ -88,6 +190,71 @@ function send() {//URL for Marvel
 
 }
 
+function attackTeam1(){
+    teamHealth1--;
+}
+
+function attackTeam2(){
+    teamHealth2--;
+}
+
+
+$("#btn-team1").on("click", function(){
+    if((gameState === "fight") && (teamHealth1 > 0) && (playerTeam === 1)){
+        attackTeam1();
+        playerClicks++;
+        console.log("Attacked Team 1 (Health: " + teamHealth1 + ")");
+        updateGameDb(teamHealth1, teamHealth2, teamHero1, teamHero2, gameHost, gameState);
+    }
+    
+});
+
+$("#btn-team2").on("click", function(){
+    if((gameState === "fight") && (teamHealth2 > 0) && (playerTeam === 2)){
+        attackTeam2();
+        playerClicks++;
+        console.log("Attacked Team 2 (Health: " + teamHealth2 + ")");
+        updateGameDb(teamHealth1, teamHealth2, teamHero1, teamHero2, gameHost, gameState);
+    }
+    
+});
+
+$("#submit-username").on("click", function(event){
+    event.preventDefault();
+    if(gameState === "initial"){
+        
+        setName($("#username-input").val().trim());
+        $("#username-input").val("");
+
+        if(gameHost === "no-host-entered"){
+            gameHost = playerKey.key;
+        }
+
+        // gameState = "fight";
+        updateGameDb(teamHealth1, teamHealth2, teamHero1, teamHero2, gameHost, gameState);
+    }
+})
+
+$(document).ready(function(){
+    // resetGame();
+});
+
+$("#check-button").on("click", function(){
+    checkGame();
+});
+
+$("#start-button").on("click", function(){
+    if (gameState === "initial" && playerKey.key === gameHost) {
+        //begin timer for fight phase
+        //wait
+        //choose hero
+        //set game values
+        //update server game values
+        //start timer for fight phase
+        console.log("Preparing to fight...");
+        setTimeout(function(){console.log("FIGHT!"); gameState = "fight";}, 3000);        
+    }
+});
 
 //test
 //Calls API function (check console)
