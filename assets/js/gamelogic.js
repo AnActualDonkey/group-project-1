bdffe6f3dd35039fbe89a4081e72ce022e2d802b
Michael <ramirez3806@gmail.com>
2019-04-30 18:25:42 -0700

    fixed

--- assets/js/gamelogic.js
+++ assets/js/gamelogic.js
@@ -297,17 +297,10 @@ $("#start-button").on("click", function () {
 
 function send() {
 
-    var spUrl = "https://gateway.marvel.com:443/v1/public/characters?orderBy=name&limit=100&apikey=" + marvelPublicCode;
+    var spUrl = "https://gateway.marvel.com:443/v1/public/characters?name=Spider-Man&apikey=" + marvelPublicCode;
     var thorUrl = "https://gateway.marvel.com:443/v1/public/characters?name=Thor&apikey=" + marvelPublicCode;
     var vultureUrl = "https://gateway.marvel.com:443/v1/public/characters?name=Vulture&apikey=" + marvelPublicCode;
-
-    // $.ajax({
-    //     url: spUrl,
-    //     method: "GET"
-
-    // }).then(function (response) {
-    //     console.log("Testing Character List: " + response);
-    // });
+    var ironmanUrl = "https://gateway.marvel.com:443/v1/public/characters?name=Iron-Man&apikey=" + marvelPublicCode;
 
     $.ajax({
         url: spUrl,
@@ -351,6 +344,23 @@ function send() {
         console.log(response);
         console.log("Typecheck: " + typeof response);
         console.log("Testing Vulture URL: " + response.data.results[0]);
+        for (key in response.data.results) {
+            console.log(key);
+        }
+        console.log("Character ID: " + response.data.results[0].id);
+        console.log("Character Name: " + response.data.results[0].name);
+        console.log("Character Description: " + response.data.results[0].description);
+        console.log("Character Thumbnail: " + response.data.results[0].thumbnail);
+        console.log("Character Image: " + response.data.results[0].thumbnail.path);
+    });
+    $.ajax({
+        url: ironmanUrl,
+        method: "GET"
+
+    }).then(function (response) {
+        console.log(response);
+        console.log("Typecheck: " + typeof response);
+        console.log("Testing Iron Man URL: " + response.data.results[0]);
         for (key in response.data.results) {
             console.log(key);
         }
